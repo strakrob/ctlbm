8d3984fe10c73415c65b88a2db9c53b4c3bd5267
hellboj <hellboj@centrum.cz>
2026-03-27 22:17:52 +0100

    local stuff

--- git_setup_pull.sh
+++ git_setup_pull.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 # initialize git repository
-git init
-git config user.name "hellboj"
-git config user.email "hellboj@centrum.cz"
-git add .
+# git init
+# git config user.name "hellboj"
+# git config user.email "hellboj@centrum.cz"
+# git add .
 
 # setup remote repository
-git remote add origin
-git remote set-url origin https://github.com/strakrob/ctlbm.git
+git remote add origin https://github.com/strakrob/ctlbm.git
 
 # pull from remote repository
 git pull origin master --allow-unrelated-histories
